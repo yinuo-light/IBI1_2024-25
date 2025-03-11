54b37295535115c306ef791fc8e494a6386da8b5
3240111004 <yinuo1.24@intl.zju.edu.cn>
2025-03-11 16:37:01 +0800

    triangle.py

---
+++ Practical4/triangle.py
@@ -0,0 +1,6 @@
+for n in range(1,11):
+#n is the number of the triangular sequence, should include the numbers from 1 to 10
+num=n*(n+1)//2
+#use this formula to caculate the triangular number
+print (num)
+#display the first ten values of the triangular numbers

38dd6202cd98856bc60f74ebc880ad5a8ba29ae3
3240111004 <yinuo1.24@intl.zju.edu.cn>
2025-03-11 16:35:47 +0800

    bmi_calculator.py

---
+++ Practical4/bmi_calculator.py
@@ -0,0 +1,14 @@
+weight=float(input("please enter your weight(kg)"))
+height=float(input("please enter your height(m)"))
+#let users imput their self information
+BMI=weight/(height**2)
+#use the formula to caculate BMI
+BMI=int(BMI)
+#get a round number
+if BMI>30:
+    print("your BMI is",BMI,", you are obese")
+elif BMI<18.5:
+    print("your BMI is",BMI,", you are underweight")
+else:
+    print("your BMI is",BMI,", you are normal weight")
+#using BMI to judge the body weight and export

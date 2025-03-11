1c68554ed66a7fbc08572e01bf60fe65aba52801
3240111004 <yinuo1.24@intl.zju.edu.cn>
2025-03-11 16:36:35 +0800

    mystery_code.py

---
+++ Practical4/mystery_code.py
@@ -0,0 +1,22 @@
+# What does this piece of code do?
+# Answer: count the whole number of the attempts until in an attempt the numbers first_n and second_n are the same
+
+# Import libraries
+# randint allows drawing a random number,
+# e.g. randint(1,5) draws a number between 1 and 5
+from random import randint
+
+# ceil takes the ceiling of a number, i.e. the next higher integer.
+# e.g. ceil(4.2)=5
+from math import ceil
+
+progress=0
+while progress>=0:
+	progress+=1
+	
+	first_n = randint(1,6)
+	second_n = randint(1,6)
+	if first_n == second_n:
+		print(progress)
+		break
+

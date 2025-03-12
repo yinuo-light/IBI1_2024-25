be9232ab91b84aa28fdab66241449d1508eacc8c
3240111004 <yinuo1.24@intl.zju.edu.cn>
2025-03-12 15:02:06 +0800

    triangle.py git commit -m triangle.py git commit -m triangle.py

--- Practical4/mystery_code.py
+++ Practical4/mystery_code.py
@@ -1,5 +1,5 @@
 # What does this piece of code do?
-# Answer: count the whole number of the attempts until in an attempt the numbers first_n and second_n are the same
+# Answer: count the whole number of the attempts until in an attempt the two numbers named first_n and second_n which randomly choose from 1 to 5 are the same
 
 # Import libraries
 # randint allows drawing a random number,

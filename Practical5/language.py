44287b0510f3c210757dfd0620196d5a19e02bdc
3240111004 <yinuo1.24@intl.zju.edu.cn>
2025-04-23 15:28:37 +0800

    language.py

--- Practical5/language.py
+++ Practical5/language.py
@@ -1,22 +1,13 @@
-
-
-#Let the users input the language they want to know the usage proportion of
-language=input("Language name:")
-
 #use the dictionary to record the information
 popularity={'JavaScript':62.3, 'HTML':52.9, 'Python':51, 'SQL':51, 'TypeScript':38.5}
-if language in popularity:
-#returns the usage proportion of the language by checking the dictionary
-   print("The percentage of people using",language,"is",popularity[language],"%")
-else:
-   print("The language is not in the list")
+print(popularity)
 
 #To draw a bar graph
 import matplotlib.pyplot as plt
 import numpy as np
 #draw a 5 columns bar graph
 N=5
-scores=(62.3, 52.9, 51, 51, 38.5)
+scores=list(popularity.values())
 ind=np.arange(N)
 width=0.35
 p1=plt.bar(ind, scores, width)
@@ -27,3 +18,11 @@ plt.xticks(ind, ('JavaScript', 'HTML', 'Python', 'SQL', 'TypeScript'))
 plt.yticks(np.arange(0,100,10))
 #show the graph 
 plt.show()
+
+#Let the users input the language they want to know the usage proportion of
+language=input("Language name:")
+if language in popularity:
+#returns the usage proportion of the language by checking the dictionary
+   print("The percentage of people using",language,"is",popularity[language],"%")
+else:
+   print("The language is not in the list")

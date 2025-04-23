0c7c40718e343f5ca5551989f797b7e618e99102
3240111004 <yinuo1.24@intl.zju.edu.cn>
2025-04-23 14:47:42 +0800

    variables.py

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -4,10 +4,17 @@ c=a+b
 d=90
 f=5
 e=d+f
-c<e
-#result： True
+c<f
+#result： False
 
 X=True
 Y=False
-X and Y
+W=X and Y
 #result： False
+
+#truth table for W:
+# X      Y      W
+# False False False
+# False True  False
+# True  False False
+# True  True  True
